8602e77042d4ccf4300910e3aaea072c3520f8f5
alirad87 <87alirad@gmail.com>
2021-01-15 11:28:02 +0330

    client

---
+++ Foodrecip/Models/Page2/Area.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foodrecip.Models.Page2
+{
+    public class Area
+    {
+        public string strArea { get; set; }
+    }
+
+    public class AreaList {
+        public List<Area> meals { get; set; }
+    }
+}
