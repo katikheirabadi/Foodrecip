459440dbaf3b7e860a9c94922d56feb3238551fc
ASUS <kati.kheirabadi@gmail.com>
2021-01-15 11:07:04 +0330

    page3

---
+++ Foodrecip/Models/Page3/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodrecip.Models.Page3
+{
+    public class Food
+    {
+        public string food { get; set; }
+        public string foodthumb { get; set; }
+        public string id { get; set; }
+    }
+}
