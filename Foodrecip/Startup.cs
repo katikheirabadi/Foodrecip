33fde4efaad3b9ea4f7433b813d363704253dbe8
alirad87 <87alirad@gmail.com>
2021-01-15 09:53:39 +0330

    inja ye alieftekhari add kardam baratun bax

--- Foodrecip/Startup.cs
+++ Foodrecip/Startup.cs
@@ -26,6 +26,8 @@ namespace Foodrecip
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //ali eftekhari
+            //sdsds
             //test
             //helooooo
             services.AddControllers();
