b94bc9386faa8d7f8a068885c121721b73298456
Mohammad <alimohammadimohammadi38@gmail.com>
2021-01-15 11:01:56 +0330

    class user page1

---
+++ Foodrecip/Services/page1/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodrecip.Services.page1
+{
+    public class User
+    {
+        public string Email { get; set; }
+        public string Favorites { get; set; }
+    }
+}
