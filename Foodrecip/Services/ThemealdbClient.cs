2d73b7345b34e90d876580dcb64a285f13ab962d
ASUS <kati.kheirabadi@gmail.com>
2021-01-15 11:50:09 +0330

    aa

--- Foodrecip/Services/ThemealdbClient.cs
+++ Foodrecip/Services/ThemealdbClient.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Foodrecip.Models.Page3;
+using System;
 using System.Net.Http;
+using System.Text.Json;
 
 namespace Foodrecip.Services
 {
@@ -15,6 +17,23 @@ namespace Foodrecip.Services
             this.client.BaseAddress = new Uri(BaseAddress);
             this.client.DefaultRequestHeaders.Add("Accept", "application/json");
         }
+
+        public FoodList GetDetail_ing(string foodname)
+        {
+            var httpresponse = client.GetAsync($"api/json/v1/1/list.php?i={foodname}").Result;
+            httpresponse.EnsureSuccessStatusCode();
+            if(!httpresponse.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            FoodList foods;
+            HttpContent httpContent = httpresponse.Content;
+            string stringcontent = httpContent.ReadAsStringAsync().Result;
+            foods = JsonSerializer.Deserialize<FoodList>(stringcontent);
+
+            return foods;
+        }
+
         
     }
 
