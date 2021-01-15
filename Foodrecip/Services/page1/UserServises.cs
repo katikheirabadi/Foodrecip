f761ab6ec7d2b123eeb2965f3de6c4b8924c67d3
Mohammad <alimohammadimohammadi38@gmail.com>
2021-01-15 11:46:32 +0330

    page1

---
+++ Foodrecip/Services/page1/UserServises.cs
@@ -0,0 +1,21 @@
+﻿using Foodrecip.Models.page1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodrecip.Services.page1
+{
+    public class UserServises
+    {
+        static List<User> us = new List<User>();
+        
+        public void Insert(User user)
+        {
+            var user2 = new User();
+            user2.Email = user.Email;
+            user2.Favorites = user.Favorites;
+            us.Add(user2);
+        }
+    }
+}
